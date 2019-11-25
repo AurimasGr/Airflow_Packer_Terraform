0a13266a6cb50edeac9a7e5d2a310133a0c9efcd
AurimasGr <griciunas.aurimas@gmail.com>
2019-11-25 09:13:09 +0200

    first commit

--- Packer/scripts/server/init_airflow.sh
+++ Packer/scripts/server/init_airflow.sh
@@ -1,2 +1,2 @@
-pip3 install apache-airflow[mysql,rabbitmq,celery]
-airflow initdb
+sudo pip3 install apache-airflow[mysql,rabbitmq,celery]
+sudo airflow initdb
--- Packer/scripts/worker/init_airflow.sh
+++ Packer/scripts/worker/init_airflow.sh
@@ -1,1 +1,1 @@
-pip3 install apache-airflow[mysql,rabbitmq,celery]
+sudo pip3 install apache-airflow[mysql,rabbitmq,celery]
