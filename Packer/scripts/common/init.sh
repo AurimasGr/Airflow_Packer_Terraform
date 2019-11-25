22ca520bb55f6144f106833bb07dd8efb0c1f97e
AurimasGr <griciunas.aurimas@gmail.com>
2019-11-25 08:35:26 +0200

    first commit

--- Packer/scripts/common/init.sh
+++ Packer/scripts/common/init.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y python3-pip
-sudo apt install build-essential libssl-dev libffi-dev python3-dev
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev
 sudo apt-get install supervisor
